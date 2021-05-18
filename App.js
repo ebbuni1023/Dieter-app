5f62a661b940117875508bdd82ad61d5be953d41
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-05-17 20:41:05 -0500

    push

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>DIETER~~~~~</Text>
+      <Text>DIETER~~~~~~~~~~</Text>
       <StatusBar style="auto" />
     </View>
   );
